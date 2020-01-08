29e5727ad8ff75d6fa19b69004dae5a48bf7fe56
Aleksandr Puhov <aleksandr_puhov@list.ru>
2020-01-09 00:57:57 +0300

    next

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import classes from './Blog.module.css';
@@ -31,6 +31,7 @@ class Blog extends Component {
         <Switch>
           <Route path='/new-post' exact component={NewPost} />
           <Route path='/posts' component={Posts} />
+          <Redirect from='/' to ='/posts'/>
           {/* <Route path='/' component={Posts} /> */}
 
         </Switch>
